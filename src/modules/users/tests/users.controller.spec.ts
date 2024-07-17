708e6d1fc068a87b0abcf9a6b3b8278696861fc1
kennethihezie <kennethihezie06@gmail.com>
2024-07-17 01:40:28 +0100

    removed createuser method

--- src/modules/users/tests/users.controller.spec.ts
+++ src/modules/users/tests/users.controller.spec.ts
@@ -29,18 +29,4 @@ describe('UsersController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
-
-  describe('createUser', () => {
-    it('should create a new user and return a response message', async () => {
-      const dto: CreateUserDto = { email: 'test@example.com', password: 'password' };
-      const userResponse: UserResponseDto = { id: '1', email: 'test@example.com' };
-
-      service.createUser.mockResolvedValue(userResponse);
-
-      const result = await controller.createUser(dto);
-
-      expect(result).toEqual(userResponse);
-      expect(service.createUser).toHaveBeenCalledWith(dto);
-    });
-  });
 });
--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,8 +1,5 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { ResponseMessage } from '../../common/decorators/response_message.decorator';
-import { AppMessages } from '../../common/constants/app_messages';
-import { CreateUserDto } from './model/dto/create-user.dto';
 import { Serialize } from '../../common/interceptors/serialize.interceptor';
 import { UserResponseDto } from './model/dto/user-response.dto';
 
@@ -11,10 +8,4 @@ import { UserResponseDto } from './model/dto/user-response.dto';
 @Controller('users')
 export class UsersController {
     constructor(private readonly service: UsersService) {}
-
-    @Post()
-    @ResponseMessage(AppMessages.USER_CREATED)
-    async createUser(@Body() dto: CreateUserDto) {
-        return await this.service.createUser(dto)
-    }
 }
